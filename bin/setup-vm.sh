eff2e93b34b64e761cd17d1be186db1f1ef9ab56
Steve Weber <steveweb@microsoft.com>
2017-10-26 23:02:32 +0000

    Fixed

--- bin/setup-vm.sh
+++ bin/setup-vm.sh
@@ -8,5 +8,11 @@ set -e
 
 apt-get install -y sshpass
 
+echo "Coping azure-linux-auto to ${HOST}..."
 sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no -r $PWD/azure-linux-auto ${USERNAME}@${HOST}:/tmp
+echo "Verifying azure-linux-auto now exists on ${HOST}..."
 sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USERNAME}@${HOST} ls -al /tmp/azure-linux-auto
+echo "Removing azure-linux-auto on ${HOST}..."
+sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USERNAME}@${HOST} rm -rf /tmp/azure-linux-auto
+echo "Verifying azure-linux-auto no longer exists on ${HOST}..."
+sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USERNAME}@${HOST} ls -al /tmp
